3730026678e9813fe7a35c3132bab34d96b545e1
DESKTOP-KTIOEGP\Murad Ahmadov <murad.ahmadov@vabiss.com>
2024-09-05 17:52:55 +0400

    - base folder structure - global error handling

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ const xss = require('xss-clean');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+const userRouter = require('./routes/userRoutes');
+const venueRouter = require('./routes/venueRoutes');
+const reservationRouter = require('./routes/reservationRoutes');
+
 const app = express();
 
 app.enable('trust proxy');
@@ -51,5 +57,16 @@ app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
+
+// 3) ROUTES
+// app.use('/api/v1/users', userRouter);
+// app.use('/api/v1/venues', venueRouter);
+// app.use('/api/v1/reservations', reservationRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
